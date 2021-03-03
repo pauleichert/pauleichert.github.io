73f4aaaff962e05bbb363719e28520b7e0beca14
Paul Eichert <pauleichert2@gmail.com>
2021-03-03 17:41:14 -0500

    Code Fixes
    Fixed color change on nav open, fixed page titles, fixed some css

--- scripts/nav-toggle.js
+++ scripts/nav-toggle.js
@@ -2,7 +2,8 @@
 $(function () {
 			$("#hamburger").click(function() {
 				$(".nav-overlay").toggleClass("d-flex");
-				$("#hamburger").toggleClass("rot45");
+				$("#hamburger").toggleClass("rot45 black-txt");
 				$(".nav-container").toggleClass("noBg");
+				$(".myName").toggleClass("black-txt");
 				});
 			})
